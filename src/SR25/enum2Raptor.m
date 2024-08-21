86fe5c75924473ee17f33c2f7afa210adfb8f178
Kosta <sergaki1@msu.edu>
2024-08-21 15:15:21 -0400

    2024a update, utilities init

---
+++ src/SR25/enum2Raptor.m
@@ -0,0 +1,9 @@
+%This script enables MATLAB enumerations to interact with the Raptor 
+%calibration interface
+
+function RaptorEnum = enum2Raptor(enum)
+[m,s] = enumeration(enum);
+datatype = 'int16';
+nums = cast(m,datatype);
+RaptorEnum = struct('Name',s,'Value',num2cell(nums));
+end
