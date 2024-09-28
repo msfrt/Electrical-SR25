fb83526b5a1ae59fb47f3b07e9ef062c01c550ea
adityapandey1221 <aditya.pandey1221@gmail.com>
2024-09-28 11:40:50 -0400

    Implemented Driver Display SOC

--- src/Embedded/DD/LightBarSOC.hpp
+++ src/Embedded/DD/LightBarSOC.hpp
@@ -3,6 +3,8 @@
 
 #include <Adafruit_NeoPixel.h>
 #include <EasyTimer.h>
+#include <FlexCAN_T4.h>
+#include <StateCAN.h>
 
 #include "CAN/CAN1.hpp"
 #include "CAN/CAN2.hpp"
@@ -11,20 +13,20 @@
 
 class LightBarSOC : public LightBar {
     private:
-    //float soc_signal_;  ///< The SOC signal
-    int min_;  ///< The minimum bound for the RPM bar to start lighting up
-    int max_;  ///< The maximum bound for the rpm bar light up before signalling
+    
+    int min_;  ///< The minimum bound for the SOC bar to start lighting up
+    int max_;  ///< The maximum bound for the SOC bar light up before signalling
              ///< an upshift
 
     protected:
     public:
     unsigned long start = 0;
-    int count_1 = 0;
-    int count_2 = 0;
-    float soc_signal_;
+    int count = 0;
+    //StateSignal & soc_signal_;
+    StateSignal & soc_signal_;
     /** Constructor */
     LightBarSOC(Adafruit_NeoPixel &lights, int first_index, int num_leds,
-              float soc_signal, int min_soc, int max_soc);
+              StateSignal & soc_signal, int min_soc, int max_soc);
 
     /** Destructor */
     virtual ~LightBarSOC(){};
@@ -42,7 +44,7 @@ class LightBarSOC : public LightBar {
 };
 
 LightBarSOC::LightBarSOC(Adafruit_NeoPixel &lights, int first_index, int num_leds,
-              float soc_signal, int min_soc, int max_soc) 
+              StateSignal & soc_signal, int min_soc, int max_soc) 
               
         : LightBar(lights, first_index, num_leds),
         soc_signal_(soc_signal),
@@ -57,36 +59,24 @@ LightBarSOC::LightBarSOC(Adafruit_NeoPixel &lights, int first_index, int num_led
 void LightBarSOC::Initialize() { LightBar::Initialize();}
 
 void LightBarSOC::Update(unsigned long & elapsed) {
+    // Reset the LEDs from the previous battery percentage value
+    for (int i = GetFirstLEDIndex(); i <= GetLastLEDIndex(); i++) {
+        lights_.setPixelColor(i, 0, 0, 0);
+    }
 
-    //Color implementation goes here
-    // if(elapsed && soc_signal_ <= 1) {
-    //     soc_signal_ += 0.001;
-    //     //start = elapsed;
-    //     Serial.println(soc_signal_);
-    //     if(elapsed == 2) {
-    //         count_2 += 1;
-    //     } else {
-    //         count_1 += 1;
-    //     }
-    //     Serial.println(count_1);
-    //     Serial.println(count_2);
-    // }
     if(elapsed == 2) {
-        count_2 += 1;
+        count += 1;
     }
-    if(count_2 % 100 == 0 && soc_signal_ <= 1) {
-        soc_signal_ += 0.01;
-        //start = elapsed;
-        Serial.println(soc_signal_);
-    }
-    int no_of_leds = soc_signal_ * GetLastLEDIndex();
+    
+    float value = soc_signal_.value() / 5;
+    int no_of_leds = value * GetLastLEDIndex();
     for (int led = GetFirstLEDIndex(); led <= GetLastLEDIndex(); led++) {
-        if(soc_signal_ >= 0.80) {
+        if(value >= 0.80) {
             //green
             if(led <= no_of_leds) {
                 lights_.setPixelColor(led, 0, 255, 0);
             }
-        } else if(soc_signal_ < 0.80 && soc_signal_ >= 0.21) {
+        } else if(value < 0.80 && value >= 0.21) {
             //orange
             if(led <= no_of_leds) {
                 lights_.setPixelColor(led, 255, 165, 0);
