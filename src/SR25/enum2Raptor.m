7541ffe67112a111122f06af6763b104213de103
Kosta <sergaki1@msu.edu>
2024-10-27 23:20:20 -0400

    Enum fix

--- src/SR25/enum2Raptor.m
+++ src/SR25/enum2Raptor.m
@@ -2,9 +2,12 @@
 %calibration interface
 
 function RaptorEnum = enum2Raptor(enum)
-[m,s] = enumeration(enum);
-datatype = 'int16';
-nums = cast(m,datatype);
-% RaptorEnum = struct('Name',s,'Value',num2cell(nums));
-RaptorEnum = struct('Name',s,'Value',nums);
+    [m,s] = enumeration(enum);
+    datatype = 'int32';
+    nums = cast(m,datatype);
+    % RaptorEnum = struct('Name',s,'Value',num2cell(nums));
+    RaptorEnum = struct('Name',transpose(s),'Value',transpose(nums));
+    % RaptorEnum = struct('Name',s,'Value',nums);
 end
+
+% struct('Name', {'No Error', 'Out of Range High', 'Out of Range Low', 'Plausibility Error'}, 'Value', [-1, 10, 11, 12])
