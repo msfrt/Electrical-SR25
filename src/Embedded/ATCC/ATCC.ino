545d296569ab8133fa5c744863e9781b8650cdae
Carter Ross <carter.h.ross.5176@gmail.com>
2025-05-13 13:29:38 -0500

    changed pin definition and working on rpm calculation

--- src/Embedded/ATCC/ATCC.ino
+++ src/Embedded/ATCC/ATCC.ino
@@ -17,7 +17,7 @@ static CAN_message_t msg;
 #include "CAN/CAN2.hpp"
 
 // ATCC Module Select - 0 front, 1 back
-const int ATCCMS = 1;
+const int ATCCMS = 0;
 
 // sensor definitions
 #include "sensors.hpp"
@@ -43,10 +43,10 @@ FreqMeasureMulti freq2;
 FreqMeasureMulti freq3;
 FreqMeasureMulti freq4;
 
-#define freq_pin1 4
-#define freq_pin2 5
-#define freq_pin3 6
-#define freq_pin4 22
+#define freq_pin1 5
+#define freq_pin2 6
+#define freq_pin3 7
+#define freq_pin4 8
 
 void setup() {
 
@@ -84,6 +84,10 @@ float sum1=0, sum2=0, sum3=0, sum4=0;
 int count1=0, count2=0, count3=0, count4=0;
 elapsedMillis timeout;
 
+float frequencyToRPM(float freq, int teeth) {
+  return (freq * 60.0) / teeth;
+}
+
 void loop() {
 
   msu_pixels(GLO_neopixel);
@@ -105,55 +109,63 @@ void loop() {
 
   if (freq1.available()) {
     sum1 = sum1 + freq1.read();
+    Serial.println(1);
     count1 = count1 + 1;
   }
   if (freq2.available()) {
     sum2 = sum2 + freq2.read();
+    Serial.println(2);
     count2 = count2 + 1;
   }
   if (freq3.available()) {
     sum3 = sum3 + freq3.read();
+    Serial.println(3);
     count3 = count3 + 1;
   }
   if (freq4.available()) {
     sum4 = sum4 + freq4.read();
+    //Serial.println(4);
     count4 = count4 + 1;
   }
   // print results every half second
   if (timeout > 500) {
     if (count1 > 0) {
-      Serial.print(freq1.countToFrequency(sum1 / count1));
+      float freq = freq1.countToFrequency(sum1 / count1);
+      Serial.print(frequencyToRPM(freq, 40));
     } else {
       Serial.print("(no pulses)");
     }
     Serial.print(",  ");
+    
     if (count2 > 0) {
-      Serial.print(freq2.countToFrequency(sum2 / count2));
+      float freq = freq2.countToFrequency(sum2 / count2);
+      Serial.print(frequencyToRPM(freq, 40));
     } else {
       Serial.print("(no pulses)");
     }
     Serial.print(",  ");
+    
     if (count3 > 0) {
-      Serial.print(freq3.countToFrequency(sum3 / count3));
+      float freq = freq3.countToFrequency(sum3 / count3);
+      Serial.print(frequencyToRPM(freq, 40));
     } else {
       Serial.print("(no pulses)");
     }
     Serial.print(",  ");
+    
     if (count4 > 0) {
-      Serial.print(freq4.countToFrequency(sum4 / count4));
+      float freq = freq4.countToFrequency(sum4 / count4);
+      Serial.print(frequencyToRPM(freq, 40));
     } else {
       Serial.print("(no pulses)");
     }
     Serial.println();
-    sum1 = 0;
-    sum2 = 0;
-    sum3 = 0;
-    sum4 = 0;
-    count1 = 0;
-    count2 = 0;
-    count3 = 0;
-    count4 = 0;
+
+    // Reset for next interval
+    sum1 = sum2 = sum3 = sum4 = 0;
+    count1 = count2 = count3 = count4 = 0;
     timeout = 0;
   }
+
 
 }
